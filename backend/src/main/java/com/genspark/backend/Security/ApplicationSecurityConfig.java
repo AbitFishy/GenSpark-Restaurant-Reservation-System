726c5908643d242ef11d408ca7340101e630f30a
kevinlin95 <contact.kevinlin95@gmail.com>
2022-05-23 21:40:49 -0400

    added user login details for us username is your name "kevinlin" password is "password" added passwordencryption

---
+++ backend/src/main/java/com/genspark/backend/Security/ApplicationSecurityConfig.java
@@ -0,0 +1,77 @@
+package com.genspark.backend.Security;
+
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+        import org.springframework.security.core.userdetails.User;
+        import org.springframework.security.core.userdetails.UserDetails;
+        import org.springframework.security.core.userdetails.UserDetailsService;
+        import org.springframework.security.crypto.password.PasswordEncoder;
+        import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    @Bean // Adding students
+    protected UserDetailsService userDetailsService() {
+        UserDetails josephTharpeUser = User.builder()
+                .username("josephtharpe")
+                .password(passwordEncoder.encode("password"))
+                .roles("QA") // Role
+                .build();
+        UserDetails taeKimUser = User.builder()
+                .username("taekim")
+                .password(passwordEncoder.encode("password"))
+                .roles("LEAD") // Role
+                .build();
+        UserDetails robertKowalczykUser = User.builder()
+                .username("robertkowalczyk")
+                .password(passwordEncoder.encode("password"))
+                .roles("DEV") // Role
+                .build();
+        UserDetails jatinPatelUser = User.builder()
+                .username("jatinpatel")
+                .password(passwordEncoder.encode("password"))
+                .roles("DEV") // Role
+                .build();
+        UserDetails kevinLinUser = User.builder()
+                .username("kevinlin")
+                .password(passwordEncoder.encode("password"))
+                .roles("DEV") // Role
+                .build();
+        return new InMemoryUserDetailsManager(
+                josephTharpeUser,
+                taeKimUser,
+                robertKowalczykUser,
+                jatinPatelUser,
+                kevinLinUser
+        );
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                // WhiteListing URL's below
+                .antMatchers("/","index","/css/*","/js*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
