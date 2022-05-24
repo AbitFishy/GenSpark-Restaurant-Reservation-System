1066019ed98d3e19ebeff16f5944b6b0399fb59f
kevinlin95 <contact.kevinlin95@gmail.com>
2022-05-24 18:46:25 -0400

    5/24

--- backend/src/main/java/com/genspark/backend/Controller/Controller.java
+++ backend/src/main/java/com/genspark/backend/Controller/Controller.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
@@ -85,4 +86,5 @@ public class Controller {
     {
         return this.reservationService.deleteReservationById(Long.parseLong(reservationID));
     }
+
 }
