6e5893baf15e16d4858ece5835ccfbc84bab8687
Robert C. Kowalczyk <catdogramb@gmail.com>
2022-05-25 14:39:26 -0400

    updated some function names

--- backend/src/main/java/com/genspark/backend/Service/EmailServiceImpl.java
+++ backend/src/main/java/com/genspark/backend/Service/EmailServiceImpl.java
@@ -24,15 +24,15 @@ public class EmailServiceImpl implements EmailService{
 
     @Override
     public void sendReservationReminder(Reservation reservation) {
-        var user = reservation.getReservationHolder();
-        LocalDateTime timeDate = LocalDateTime.from(reservation.getTimeDate());
+        var user = reservation.getUserAccount();
+        LocalDateTime timeDate = LocalDateTime.from(reservation.getDateTime());
         StringBuilder sb = new StringBuilder();
         sb.append("Hello ").append(user.getPrimaryName()).append(". You have a reservation at Restaurant ");
         sb.append(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).format(timeDate)).append(" at ");
         sb.append(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).format(timeDate));
         sb.append("\n\nIf you need to change your reservation or cancel, please visit our website at localhost:8080/reservations");
         sb.append("or call us at 555-555-5555.\n\nThank you for eating at Restaurant!");
-        sendEmail(reservation.getReservationHolder().getEmail(), reservationReminderTitle, sb.toString());
+        sendEmail(reservation.getUserAccount().getEmail(), reservationReminderTitle, sb.toString());
     }
 
     @Override
