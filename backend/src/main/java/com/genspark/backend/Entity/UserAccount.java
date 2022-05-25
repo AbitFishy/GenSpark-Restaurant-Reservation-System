0ef78ad962fbd1b638c93b506e0418050f355a10
kevinlin95 <contact.kevinlin95@gmail.com>
2022-05-25 11:46:18 -0400

    deleted setters and getters

--- backend/src/main/java/com/genspark/backend/Entity/UserAccount.java
+++ backend/src/main/java/com/genspark/backend/Entity/UserAccount.java
@@ -1,8 +1,16 @@
 package com.genspark.backend.Entity;
 
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tbl_userAccounts")
 public class UserAccount {
 
@@ -19,63 +27,6 @@ public class UserAccount {
     @Column(name = "email", unique = true, nullable = false)
     private String email;
 
-    public UserAccount() {
-    }
-
-    public UserAccount(String primaryName,
-                       String phoneNumber,
-                       String password,
-                       String email) {
-        this.primaryName = primaryName;
-        this.phoneNumber = phoneNumber;
-        this.password = password;
-        this.email = email;
-    }
-
-    public UserAccount(String password, String email) {
-        this.password = password;
-        this.email = email;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getPrimaryName() {
-        return primaryName;
-    }
-
-    public void setPrimaryName(String primaryName) {
-        this.primaryName = primaryName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     @Override
     public String toString() {
