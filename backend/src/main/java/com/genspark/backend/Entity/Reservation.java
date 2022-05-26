23fdc24b555523c88cf8cecac5023c33dfed4b91
tkim013 <tkim013@gmail.com>
2022-05-26 17:24:16 -0400

    dev branch fix again

--- backend/src/main/java/com/genspark/backend/Entity/Reservation.java
+++ backend/src/main/java/com/genspark/backend/Entity/Reservation.java
@@ -2,39 +2,91 @@ package com.genspark.backend.Entity;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
-@Data
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name = "tbl_reservations")
+@Table(name = "reservations")
 public class Reservation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "reservation_id", nullable = false)
-    private long reservationId;
+    private long resId;
 
-
-    @Column(nullable = false)
+    @Column(name="date", nullable = false)
     private String dateTime;
 
     @Column(nullable = false)
     private int numberOfGuests;
 
-    @Column(name = "status")
     private StatusType type;
 
+    public Reservation(String dateTime, int numberOfGuests, StatusType type, String resName, String resNumber) {
+        this.dateTime = dateTime;
+        this.numberOfGuests = numberOfGuests;
+        this.type = type;
+        this.resName = resName;
+        this.resNumber = resNumber;
+    }
+
+    @Column(name = "primaryName", nullable = false)
     private String resName;
 
+    @Column(name = "phoneNumber", nullable = false)
     private String resNumber;
+
+    public Reservation() {
+    }
+
+    public String getResName() {
+        return resName;
+    }
+
+    public void setResName(String resName) {
+        this.resName = resName;
+    }
+
+    public String getResNumber() {
+        return resNumber;
+    }
+
+    public void setResNumber(String resNumber) {
+        this.resNumber = resNumber;
+    }
+
+    public long getResId() {
+        return resId;
+    }
+
+    public void setResId(long resId) {
+        this.resId = resId;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public void setNumberOfGuests(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public StatusType getType() {
+        return type;
+    }
+
+    public void setType(StatusType type) {
+        this.type = type;
+    }
 
     public enum StatusType {
         TYPE1("PENDING"), TYPE2("CONFIRMED"),
@@ -57,8 +109,13 @@ public class Reservation {
         }
     }
 
-//    @OneToOne
-//    @MapsId
-//    private UserAccount userAccount;
-
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "reservationId=" + resId +
+                ", dateTime='" + dateTime + '\'' +
+                ", numberOfGuests=" + numberOfGuests +
+                ", type=" + type +
+                '}';
+    }
 }
