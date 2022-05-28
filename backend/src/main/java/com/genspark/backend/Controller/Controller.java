6a39dcbc814b891d8bbd5e742acfda69b794613f
kevinlin95 <contact.kevinlin95@gmail.com>
2022-05-28 14:06:01 -0400

    Separated Controllers into two Management classes One for User, another for Reservations Moved QR code to Web Controller Added @PreAuthorize Annotations for Users Reservations still need @PreAuthorize added

--- backend/src/main/java/com/genspark/backend/Controller/Controller.java
+++ backend/src/main/java/com/genspark/backend/Controller/Controller.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,82 +23,11 @@ public class Controller {
 
     final Logger logger = LoggerFactory.getLogger(Controller.class);
 
-    @Autowired
-    private UserAccountService userAccountService;
 
-    @Autowired
-    private ReservationService reservationService;
 
 //    @Autowired
 //    private EmailService emailService;
-
-    @GetMapping("/")
-    public String home() {
-        return "Hi there.";
-    }
-
-    @GetMapping("/user")
-    public List<UserAccount> getUserAccounts() {
-        return this.userAccountService.getAllUserAccount();
-    }
-
-    @GetMapping("/userAccounts/{userID}")
-    public UserAccount getUserAccount(@PathVariable String userID) {
-        return this.userAccountService.getUserAccountById(Long.parseLong(userID));
-    }
-
-    @PostMapping("/user")
-    public UserAccount addUserAccount(@RequestBody UserAccount userAccount) {
-        return this.userAccountService.addUserAccount(userAccount);
-    }
-
-    @PostMapping("/login")
-    public UserAccount login(@RequestBody UserAccount userAccount) {
-        return this.userAccountService.login(userAccount);
-    }
-
-    @PostMapping("/auth")
-    public String authenticateUserAccount(@RequestBody String email, @RequestBody String clearPassword) {
-        return this.userAccountService.authenticateUserAccount(email, clearPassword) ? "true" : "false";
-    }
-
-    @PutMapping("/userAccounts/{userID}")
-    public UserAccount updateUserAccount(@RequestBody UserAccount userAccount, @PathVariable Long userID) {
-        return this.userAccountService.updateUserAccount(userAccount, userID);
-    }
-
-    @DeleteMapping("/userAccounts/{userID}")
-    public String deleteAccount(@PathVariable String userID)
-    {
-        return this.userAccountService.deleteUserAccountById(Long.parseLong(userID));
-    }
-
-    @GetMapping("/reservation")
-    public List<Reservation> getReservations() {
-        return this.reservationService.getAllReservation();
-    }
-
-    @GetMapping("/reservation/{reservationID}")
-    public Reservation getReservation(@PathVariable String reservationID) {
-        return this.reservationService.getReservationById(Long.parseLong(reservationID));
-    }
-
-    @PostMapping("/reservation")
-    public Reservation addReservation(@RequestBody Reservation reservation) {
-        return this.reservationService.addReservation(reservation);
-    }
-
-    @PutMapping("/reservation/{reservationID}")
-    public Reservation updateReservation(@RequestBody Reservation reservation, @PathVariable Long reservationID) {
-        return this.reservationService.updateReservation(reservation, reservationID);
-    }
-
-    @DeleteMapping("/reservation/{reservationID}")
-    public String deleteReservation(@PathVariable String reservationID)
-    {
-        return this.reservationService.deleteReservationById(Long.parseLong(reservationID));
-    }
-
+//
 //    @GetMapping("/dev/testing/email")
 //    public String sendTestEmail(){
 //        return emailService.sendEmail("catdogramb@gmail.com",
@@ -108,25 +38,4 @@ public class Controller {
 //                :
 //                "Error while sending email";
 //    }
-
-    // 2fa
-    // http://localhost:8080/generateQRCode/code/350/350
-    @GetMapping(value = "/generateQRCode/{codeText}/{width}/{height}")
-    public ResponseEntity<byte[]> generateQRCode(
-            @PathVariable("codeText") String codeText,
-            @PathVariable("width") Integer width,
-            @PathVariable("height") Integer height)
-            throws Exception {
-        return ResponseEntity.status(HttpStatus.OK).body(QRCodeGenerator.getQRCodeImage(codeText, width, height));
-    }
-    // http://localhost:8080/generateAndDownloadQRCode/code/350/350
-    private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/QRCode.png";
-    @GetMapping(value = "/generateAndDownloadQRCode/{codeText}/{width}/{height}")
-    public void download(
-            @PathVariable("codeText") String codeText,
-            @PathVariable("width") Integer width,
-            @PathVariable("height") Integer height)
-            throws Exception {
-        QRCodeGenerator.generateQRCodeImage(codeText, width, height, QR_CODE_IMAGE_PATH);
-    }
 }
---
+++ backend/src/main/java/com/genspark/backend/Controller/UserManagementController.java
@@ -0,0 +1,60 @@
+package com.genspark.backend.Controller;
+
+import com.genspark.backend.Entity.UserAccount;
+import com.genspark.backend.Service.UserAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("management/api/users")
+public class UserManagementController {
+    @Autowired
+    private UserAccountService userAccountService;
+
+    @GetMapping("/user")
+    // using 'ROLE_ --> because of ApplicationUserRole class --> method getGrantedAuthority()
+    @PreAuthorize("hasAnyRole('ROLE_EMPLOY', 'ROLE_ADMIN')")
+    public List<UserAccount> getUserAccounts() {
+        return this.userAccountService.getAllUserAccount();
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_EMPLOY', 'ROLE_ADMIN', 'ROLE_USER')")
+    @GetMapping("/userAccounts/{userID}")
+    public UserAccount getUserAccount(@PathVariable String userID) {
+        return this.userAccountService.getUserAccountById(Long.parseLong(userID));
+    }
+
+    @PostMapping("/user")
+    @PreAuthorize("hasAuthority('admin:create', 'employ:create')")
+    public UserAccount addUserAccount(@RequestBody UserAccount userAccount) {
+        return this.userAccountService.addUserAccount(userAccount);
+    }
+
+    @PostMapping("/login")
+    @PreAuthorize("hasAuthority('admin:create', 'employ_create', 'user_create')")
+    public UserAccount login(@RequestBody UserAccount userAccount) {
+        return this.userAccountService.login(userAccount);
+    }
+
+    @PostMapping("/auth")
+    @PreAuthorize("hasAuthority('admin:create', 'employ:create', 'user:create')")
+    public String authenticateUserAccount(@RequestBody String email, @RequestBody String clearPassword) {
+        return this.userAccountService.authenticateUserAccount(email, clearPassword) ? "true" : "false";
+    }
+
+    @PutMapping("/userAccounts/{userID}")
+    @PreAuthorize("hasAuthority('admin:edit', 'employ:edit')")
+    public UserAccount updateUserAccount(@RequestBody UserAccount userAccount, @PathVariable Long userID) {
+        return this.userAccountService.updateUserAccount(userAccount, userID);
+    }
+
+    @DeleteMapping(path="{userID}")
+    @PreAuthorize("hasAuthority('admin:delete', 'employ:delete')")
+    public String deleteAccount(@PathVariable String userID)
+    {
+        return this.userAccountService.deleteUserAccountById(Long.parseLong(userID));
+    }
+}
