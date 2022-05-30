b68117d0141e6b16274227048149df740b7dfed6
kevinlin95 <contact.kevinlin95@gmail.com>
2022-05-30 16:09:28 -0400

    5/30 Remerged Controllers updated authorities for users/reservations

--- backend/src/main/java/com/genspark/backend/Controller/Controller.java
+++ backend/src/main/java/com/genspark/backend/Controller/Controller.java
@@ -21,21 +21,89 @@ import java.util.List;
 @RequestMapping(path = "api")
 public class Controller {
 
+    @Autowired
+    private UserAccountService userAccountService;
+    @Autowired
+    private ReservationService reservationService;
+
     final Logger logger = LoggerFactory.getLogger(Controller.class);
 
+    @GetMapping("/user")
+    // using 'ROLE_ --> because of ApplicationUserRole class --> method getGrantedAuthority()
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    public List<UserAccount> getUserAccounts() {
+        return this.userAccountService.getAllUserAccount();
+    }
+
+    @GetMapping("/userAccounts/{userID}")
+    @PreAuthorize("hasAnyRole('ROLE_EMPLOY', 'ROLE_ADMIN')")
+    public UserAccount getUserAccount(@PathVariable String userID) {
+        return this.userAccountService.getUserAccountById(Long.parseLong(userID));
+    }
+
+    @PostMapping("/user")
+    @PreAuthorize("hasAuthority('admin:create')")
+    public UserAccount addUserAccount(@RequestBody UserAccount userAccount) {
+        return this.userAccountService.addUserAccount(userAccount);
+    }
+
+    @PostMapping("/login")
+    @PreAuthorize("hasAuthority('admin:create', 'employ_create')")
+    public UserAccount login(@RequestBody UserAccount userAccount) {
+        return this.userAccountService.login(userAccount);
+    }
+
+    @PostMapping("/auth")
+    @PreAuthorize("hasAuthority('admin:create', 'employ:create')")
+    public String authenticateUserAccount(@RequestBody String email, @RequestBody String clearPassword) {
+        return this.userAccountService.authenticateUserAccount(email, clearPassword) ? "true" : "false";
+    }
+
+    @PutMapping("/userAccounts/{userID}")
+    @PreAuthorize("hasAuthority('admin:edit')")
+    public UserAccount updateUserAccount(@RequestBody UserAccount userAccount, @PathVariable Long userID) {
+        return this.userAccountService.updateUserAccount(userAccount, userID);
+    }
+
+    @DeleteMapping(path="{userID}")
+    @PreAuthorize("hasAuthority('admin:delete')")
+    public String deleteAccount(@PathVariable String userID)
+    {
+        return this.userAccountService.deleteUserAccountById(Long.parseLong(userID));
+    }
+
+//--------------------------------------------------------------------------------------------------------------------//
+    @GetMapping("/reservation")
+    @PreAuthorize("hasAnyRole('ROLE_EMPLOY', 'ROLE_ADMIN')")
+    public List<Reservation> getReservations() {
+        return this.reservationService.getAllReservation();
+    }
+
+    @GetMapping("/reservation/{reservationID}")
+    @PreAuthorize("hasAnyRole('ROLE_EMPLOY', 'ROLE_ADMIN', 'ROLE_USER')")
+    public Reservation getReservation(@PathVariable String reservationID) {
+        return this.reservationService.getReservationById(Long.parseLong(reservationID));
+    }
+
+    @PostMapping("/reservation")
+    @PreAuthorize("hasAuthority('admin:create', 'employ:create')")
+    public Reservation addReservation(@RequestBody Reservation reservation) {
+        return this.reservationService.addReservation(reservation);
+    }
+
+    @PutMapping("/reservation/{reservationID}")
+    @PreAuthorize("hasAuthority('admin:edit', 'employ:edit')")
+    public Reservation updateReservation(@RequestBody Reservation reservation, @PathVariable Long reservationID) {
+        return this.reservationService.updateReservation(reservation, reservationID);
+    }
+
+    @DeleteMapping("/reservation/{reservationID}")
+    @PreAuthorize("hasAuthority('admin:delete', 'employ:delete')")
+    public String deleteReservation(@PathVariable String reservationID)
+    {
+        return this.reservationService.deleteReservationById(Long.parseLong(reservationID));
+    }
 
 
-//    @Autowired
-//    private EmailService emailService;
-//
-//    @GetMapping("/dev/testing/email")
-//    public String sendTestEmail(){
-//        return emailService.sendEmail("catdogramb@gmail.com",
-//                "Test from Restaurant",
-//                "this was a test message")
-//                ?
-//                "Successfully sent email"
-//                :
-//                "Error while sending email";
-//    }
+
 }
