0feaa8f2f393ef9d704ce713d4d05fa1876f4a5d
Jatin Patel <jatinjay10@gmail.com>
2022-06-09 10:10:44 -0400

    working text sender

--- backend/src/main/java/com/genspark/backend/Service/TextServiceImpl.java
+++ backend/src/main/java/com/genspark/backend/Service/TextServiceImpl.java
@@ -3,15 +3,19 @@ package com.genspark.backend.Service;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class TextServiceImpl {
     // Find your Account Sid and Token at twilio.com/console
-    public static final String ACCOUNT_SID = "AC231db42124c05a0a3a4a454b875d0bfb";
-    public static final String AUTH_TOKEN = "849b464694e094528975b264de4fb8fe";
+    static Dotenv dotenv = Dotenv.configure()
+            .directory("src/main/resources")
+            .filename("twilio.env")
+            .load();
 
     public static boolean sendSMS(String number, String text) {
+        System.out.println(System.getProperty("user.dir"));
         try{
-            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+            Twilio.init(dotenv.get("TWILIO_ACCOUNT_SID"), dotenv.get("TWILIO_AUTH_TOKEN"));
             Message message = Message.creator(
                             new PhoneNumber(number),
                             "MGb89b592734846740498f96a9a8e54979",
@@ -20,7 +24,9 @@ public class TextServiceImpl {
 
             return true;
         }catch (Exception e){
+            e.printStackTrace();
             return false;
         }
     }
+
 }
