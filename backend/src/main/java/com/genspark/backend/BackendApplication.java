2fed3caf9e33ba5a97bd38e19283ac1cb430f4f1
Jatin Patel <jatinjay10@gmail.com>
2022-06-14 14:40:04 -0400

    Update BackendApplication.java

--- backend/src/main/java/com/genspark/backend/BackendApplication.java
+++ backend/src/main/java/com/genspark/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.genspark.backend;
 
+import com.genspark.backend.Service.TextServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		TextServiceImpl.sendSMS("8564057466","tett3");
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
